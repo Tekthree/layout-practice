ae6e2254a58f6ea911f86a35b8c778d540e3a2d7
Tek Jones <tekthree@gmail.com>
2022-12-07 08:35:40 -0800

    finishes css for footer and adds responsive css, adds js for menu open close

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 'use strict';
 
+//Get elements from the DOM
+
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+
+// toggle open close button
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu-open');
+});
